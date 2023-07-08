41cad027bc7a2137672cfcca354b6a86198e7a60
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-07-08 23:48:44 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+const App =   (
+    <div className = "app">
+    <button onClick="">Click Me</button>
+    </div>
+  )
+
+const container = document.getElementById('app');
+const root = ReactDOM.createRoot(container);
+root.render(App)
