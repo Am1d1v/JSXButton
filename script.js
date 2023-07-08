af55ff0c9ab5fda3524d4995076c2d2b25c3c2d2
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-07-09 00:14:27 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 
 
-let BtnText = 'Click Here';
 
-const onBtnClick = () => {
-    BtnText = 'React Hello'
-    console.log(BtnText);
+
+const App = () => {
+
+    console.log('Called');
+
+    let BtnText = 'Click Here';
+
+    const onBtnClick = () => {
+        BtnText = 'React Hello'
+        console.log(BtnText);
 };
 
-const App =   (
-    <div className = "app">
-    <button onClick={onBtnClick}>{BtnText}</button>
-    </div>
-  )
+    return (
+        <div className = "app">
+        <button onClick={onBtnClick}>{BtnText}</button>
+        </div>
+      )
+}
 
 const container = document.getElementById('app');
 const root = ReactDOM.createRoot(container);
-root.render(App)
+root.render(<App />)
